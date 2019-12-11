953c33b022c93d70d90013f5891b77578340dde8
esoft16 - Henrique Akiyoshi Eguchi Pires Ribeiro - Grupo 4 <henrique.eguchi@outlook.com>
2019-12-11 14:24:02 -0300

    Configura o repositório incluindo o projeto base para o desafio

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # easynvest-test
+
+Test link:
+
+https://github.com/easynvest/teste-ios
+
+
+Progress log:
+
+Dec 2nd
+
+2:00PM ~ 2:15PM - Project startup (git repo/xcode proj)
+
+--
+
+
---
+++ easynvest-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easynvest-test
+//
+//  Created by Henrique Akiyoshi Eguchi on 11/12/19.
+//  Copyright © 2019 Henrique Akiyoshi Eguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
